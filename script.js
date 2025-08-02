dc1022a4978aa7ad58c81261287b7bb56951e68b
Yonatan Wolde <yonatanwolde251@gmail.com>
2025-08-02 23:40:21 +0300

    second time update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Create remove button
         const removeBtn = document.createElement('button');
         removeBtn.textContent = 'Remove';
-        removeBtn.className = 'remove-btn';
+        removeBtn.classList.add('remove-btn'); // Use classList.add instead of className
 
         // Set remove button click event to remove task
         removeBtn.onclick = function () {
